4171e553f56ce047dc677c93ed0bb40aa4b1f96f
JJmeh <35524342+JJmeh@users.noreply.github.com>
2025-02-18 16:25:32 +0000

    Build: (ba97b63) update

--- lib/font.ts
+++
@@ -1,7 +0,0 @@
-import localFont from 'next/font/local'
-
-export const geistSans = localFont({
-    src: '../app/fonts/GeistVF.woff',
-    variable: '--font-geist-sans',
-    weight: '100 200 300 400 500 600 700 800 900'
-})
